898808c911ef1f7b33a995bf044fb9cb5b8ebd8c
Gonzalo Sáez Martí <Gonzalo Sez Mart@192.168.145.131>
2018-11-19 19:16:48 +0100

    Creada la pagina inicio.

---
+++ inicio.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    </head>
+    <body style="background-color: #000">
+        <div id="padre">
+            <br>
+            <div id="padresitoC" class="row" style="background-color: #ff9933">
+                <div class="col-5"></div>
+                <div class="col-6">dddddddddd</div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div id="Buscador" class="row">
+            <div class="col-12">
+                <input class="typeahead form-control" type="text" placeholder="Buscar">
+            </div>
+        </div>
+        <div class="container" id="Principal" style="clear: both; background-color: #ffffff">
+
+            <br>
+            <br>
+            <div id="Contenido" class="row">
+                <div class="col-2"></div>
+                <div class="col-8">
+                    <div style="background-color: #00ff00">sefefsefwsefs</div>
+                    <button class="btn btn-primary" style="margin-left: 90%">Añadir</button>
+                </div>
+                <div class="col-2"></div>
+            </div>
+        </div>
+
+    </body>
+    <script src="js/jquery-1.11.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
+    <script>
+
+    </script>
+</html>
