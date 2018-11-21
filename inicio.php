c0fb180103d691a663a85da99d705a641cf66e39
Gonzalo Sáez Martí <Gonzalo Sez Mart@192.168.145.131>
2018-11-21 15:02:27 +0100

    Mejorada la ventana inicio y el modal.

--- inicio.php
+++ inicio.php
@@ -7,44 +7,44 @@
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
     </head>
-    <body style="background-color: #000">
-        <div id="padre">
-            <br>
-            <div id="padresitoC" class="row" style="background-color: #ff9933">
-                <div class="col-5"></div>
-                <div class="col-6">dddddddddd</div>
+    <body style="background-color: #ffde88">
+        <div id="papa" >
+            <div id = "cabecera" class="row"  style="margin: 0 auto; margin-top: 2%; border: 2px solid yellow; background-image: url('img/banner.jpg');
+                 background-size: cover;">
+                <div class="col-1"></div>
+                <div class="col-2"><a href="index.php"><img src="img/iconoa_r.png"class="img-fluid"></a></div>
+                <div class="col-6" style="display: block"> 
+                    <h2 class="text-center" style="color: white">ALMACEN DE LOS LEGIONARIOS 2018</h2>
+                </div>
             </div>
         </div>
         <br>
         <br>
-        <div id="Buscador" class="row">
-            <div class="col-12">
+        <div id="buscapadre" class="row" style="width: 100%">
+            <div class="col-4"></div>
+            <div id="buscador" class="col-4">
                 <input class="typeahead form-control" type="text" placeholder="Buscar">
             </div>
+            <div class="col-5"></div>
         </div>
+        
         <div class="container" id="Principal" style="clear: both; background-color: #ffffff">
-
-            <br>
-            <br>
             <div id="Contenido" class="row">
                 <div class="col-2"></div>
                 <div class="col-8">
-                    <div style="background-color: #00ff00">sefefsefwsefs</div>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#btnAnyadir" style="margin-left: 90.9%">
-                        Añadir
-                    </button>
+                    
                 </div>
                 <div class="col-2"></div>
             </div>
         </div>
-
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#btnAnyadir" style="margin-left: 85%">Añadir</button>
     </body>
-    
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script>
-        
+
     </script>
 
     <div class="modal fade" id="btnAnyadir" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -59,8 +59,10 @@
                 <div class="modal-body">
                     <p>Nombre:</p>
                     <input type="text" class="form-control" id="recipient-name">
+                    <br>
                     <p>Numero de Serie:</p>
                     <input type="text" class="form-control" id="recipient-name">
+                    <br>
                     <p>Cantidad:</p>
                     <input type="text" class="form-control" id="recipient-name">
                 </div>
